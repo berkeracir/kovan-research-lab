bb7302ae9c8ad9da211b65be11c5e5c4bf5d0f34
Berker ACIR <berkeracir159@gmail.com>
2018-08-13 16:57:43 +0300

    path fix

--- object_detection/k_object_classification.py
+++ object_detection/k_object_classification.py
@@ -5,15 +5,15 @@ from PIL import Image
 from natsort import natsorted
 from keras.preprocessing import image
 
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-model_name = "ResNet50_imagenet"
-model = ResNet50(weights="imagenet")
-target_size = (224, 224)
+#from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+#model_name = "ResNet50_imagenet"
+#model = ResNet50(weights="imagenet")
+#target_size = (224, 224)
 
-#from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input, decode_predictions
-#model_name = "InceptionResNetV2_imagenet"
-#model = InceptionResNetV2(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
-#target_size = (299, 299)
+from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input, decode_predictions
+model_name = "InceptionResNetV2_imagenet"
+model = InceptionResNetV2(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+target_size = (299, 299)
 
 def predict(model, img, target_size, top_n=3):
 	"""Run model prediction on image
@@ -40,17 +40,18 @@ def predict(model, img, target_size, top_n=3):
 CWD_PATH = "/".join(os.path.realpath(__file__).split('/')[:-1])
 
 PATH_TO_IMAGES_DIR = os.path.join(CWD_PATH, "input_images")
-PATH_TO_OUTPUT_DIR = os.path.join(CWD_PATH, "output")
+#PATH_TO_OUTPUT_DIR = os.path.join(CWD_PATH, "output_images", model_name)
 #if os.path.isdir(PATH_TO_OUTPUT_DIR):
 #	shutil.rmtree(PATH_TO_OUTPUT_DIR)
 #os.mkdir(PATH_TO_OUTPUT_DIR)
+PATH_TO_OUTPUT = os.path.join(CWD_PATH, "output")
 
 IMAGE_PATHS = []
 for (dirpath, dirnames, filenames) in os.walk(PATH_TO_IMAGES_DIR):
 	IMAGE_PATHS.extend(natsorted(filenames))
 	break
 
-with open(os.path.join(PATH_TO_OUTPUT_DIR, model_name + ".txt"), 'w') as fp:
+with open(os.path.join(PATH_TO_OUTPUT, model_name + ".txt"), 'w') as fp:
 	for image_path in IMAGE_PATHS:
 		img = Image.open(os.path.join(PATH_TO_IMAGES_DIR, image_path))
 		predictions = predict(model, img, target_size)
