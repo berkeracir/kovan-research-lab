cba475f1360e9b4370d50314076b0e1142d128bd
Berker ACIR <berkeracir159@gmail.com>
2018-08-28 15:46:16 +0300

    major changes

--- model_training/test/train_model.py
+++ model_training/test/train_model.py
@@ -1,4 +1,6 @@
 import os
+import errno
+import xml.etree.ElementTree as ET
 from natsort import natsorted
 
 import tensorflow as tf
@@ -9,34 +11,50 @@ IMAGES_DIR = "Images"
 RECORDS_DIR = "tfrecords"
 LABEL_PATH = "tools_label_map.pbtxt"
 
-flags = tf.app.flags
+"""flags = tf.app.flags
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
-FLAGS = flags.FLAGS
+FLAGS = flags.FLAGS"""
 
-def create_tf_example():
+def create_tf_example(wnid, image, id):
+	image_id = image.split(".")[0]
+	extension = image.split(".")[-1]
+	image_path = os.path.join(IMAGES_DIR, wnid, image)
+	image_xml = os.path.join(ANNOTATION_DIR, wnid, image_id + ".xml")
+	root = ET.parse(image_xml).getroot()
+
 	# TODO(user): Populate the following variables from your example.
-	height = 500 # Image height
-	width = 375 # Image width
-	filename = "n04154565_323" # Filename of the image. Empty if image is not from file
-	#encoded_image_data = None # Encoded image bytes
-	image_format = b'jpg' # b'jpeg' or b'png'
+	height = int(root[3][1].text) # Image height
+	width = int(root[3][0].text) # Image width
+	filename = image_path # Filename of the image. Empty if image is not from file
+	image_format = bytes(extension) # b'jpeg' or b'png'
 
-	xmins = [22.0/375.0, 0.0/375.0] # List of normalized left x coordinates in bounding box (1 per box)
-	xmaxs = [280.0/375.0, 499.0/375.0] # List of normalized right x coordinates in bounding box
+	xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
+	xmaxs = [] # List of normalized right x coordinates in bounding box
 				# (1 per box)
-	ymins = [50.0/500.0, 104.0/500.0] # List of normalized top y coordinates in bounding box (1 per box)
-	ymaxs = [210.0/500.0, 352.0/500.0] # List of normalized bottom y coordinates in bounding box
+	ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
+	ymaxs = [] # List of normalized bottom y coordinates in bounding box
 				# (1 per box)
-	classes_text = ["n04154565", "n04154565"] # List of string class name of bounding box (1 per box)
-	classes = [1, 1] # List of integer class id of bounding box (1 per box)
+	classes_text = [] # List of string class name of bounding box (1 per box)
+	classes = [] # List of integer class id of bounding box (1 per box)
+
+	for xmin in root.iter('xmin'):
+		xmins.append(float(xmin.text)/width)
+	for xmax in root.iter('xmax'):
+		xmaxs.append(float(xmax.text)/width)
+	for ymin in root.iter('ymin'):
+		ymins.append(float(ymin.text)/height)
+	for ymax in root.iter('ymax'):
+		ymaxs.append(float(ymax.text)/height)
+	for name in root.iter('name'): # TODO: this may cause error in case of different bounding box objects
+		classes_text.append(name.text)
+		classes.append(id)
 
 	tf_example = tf.train.Example(features=tf.train.Features(feature={
 		'image/height': dataset_util.int64_feature(height),
 		'image/width': dataset_util.int64_feature(width),
 		'image/filename': dataset_util.bytes_feature(filename),
 		'image/source_id': dataset_util.bytes_feature(filename),
-		#'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-		#'image/format': dataset_util.bytes_feature(image_format),
+		'image/format': dataset_util.bytes_feature(image_format),
 		'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
 		'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
 		'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
@@ -67,18 +85,34 @@ def main(_):
 			image_xml = os.path.join(ANNOTATION_DIR, wnid, image_id + ".xml")
 
 			if os.path.isfile(image_xml):
-				print image_id
+				# Create necessary directories and files for tfrecords 
+				tfrecords_path = os.path.join(RECORDS_DIR, wnid, image_id + ".tfrecords")
+				if not os.path.exists(os.path.dirname(tfrecords_path)):
+					try:
+						os.makedirs(os.path.dirname(tfrecords_path))
+					except OSError as exc:
+						if exc.errno != errno.EEXIST:
+							raise
+				open(tfrecords_path, 'a').close()
+
+				flags = tf.app.flags
+				flags.DEFINE_string('output_path', tfrecords_path, 'Path to output TFRecord')
+				FLAGS = flags.FLAGS
+				
+				writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+
+				tf_example = create_tf_example(wnid, image, id)
+				writer.write(tf_example.SerializeToString())
+
+				writer.close()
+
+				for name in list(FLAGS):
+					delattr(FLAGS, name)
 		
 		id += 1
 
 	exit()
 
-	writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-
-	tf_example = create_tf_example()
-	writer.write(tf_example.SerializeToString())
-
-	writer.close()
 
 if __name__ == '__main__':
 	tf.app.run()
